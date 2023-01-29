e00176b596f874a2c77463d604312700a6af4c15
nayuta-ai <nayuta723@gmail.com>
2023-01-29 23:28:59 +0900

    Add clone_parent and parent_child

---
+++ src/nsexec.c
@@ -0,0 +1,28 @@
+#include <setjmp.h>
+
+/* Assume the stack grows down, so arguments should be above it. */
+struct clone_t {
+	/*
+	 * Reserve some space for clone() to locate arguments
+	 * and retcode in this place
+	 */
+	char stack[4096] __attribute__((aligned(16)));
+	char stack_ptr[0];
+
+	/* There's two children. This is used to execute the different code. */
+	jmp_buf *env;
+	int jmpval;
+};
+
+static int child_func(void *arg){
+    struct clone_t *ca = (struct clone_t *)arg;
+    longjmp(*ca->env, ca->jmpval);
+}
+
+static int clone_parent(jmp_buf *env, int jmpval){
+    struct clone_t ca = {
+    .env = env,
+    .jmpval = jmpval,
+  };
+  return clone(child_func, ca.stack_ptr, CLONE_PARENT | SIGCHLD, &ca);
+}
