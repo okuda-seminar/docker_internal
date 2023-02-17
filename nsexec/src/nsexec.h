72c146a7e83274ce233706d538517b3d92c8a750
nayuta-ai <nayuta723@gmail.com>
2023-02-17 23:08:16 +0900

    Format C/C++ code according to Google C++ style

--- nsexec/src/nsexec.h
+++ nsexec/src/nsexec.h
@@ -4,30 +4,33 @@
 #include <setjmp.h>
 
 /* Assume the stack grows down, so arguments should be above it. */
-struct clone_t {
-	/*
-	 * Reserve some space for clone() to locate arguments
-	 * and retcode in this place
-	 */
-	char stack[4096] __attribute__((aligned(16)));
-	char stack_ptr[0];
+struct clone_t
+{
+  /*
+   * Reserve some space for clone() to locate arguments
+   * and retcode in this place
+   */
+  char stack[4096] __attribute__((aligned(16)));
+  char stack_ptr[0];
 
-	/* There's two children. They are used to execute the different code. */
-	jmp_buf *env;
-	int jmpval;
+  /* There's two children. They are used to execute the different code. */
+  jmp_buf *env;
+  int jmpval;
 };
 
-static int child_func(void *arg){
-    struct clone_t *ca = (struct clone_t *)arg;
-    longjmp(*ca->env, ca->jmpval);
+static int child_func(void *arg)
+{
+  struct clone_t *ca = (struct clone_t *)arg;
+  longjmp(*ca->env, ca->jmpval);
 }
 
-static int clone_parent(jmp_buf *env, int jmpval){
-    struct clone_t ca = {
-    .env = env,
-    .jmpval = jmpval,
+static int clone_parent(jmp_buf *env, int jmpval)
+{
+  struct clone_t ca = {
+      .env = env,
+      .jmpval = jmpval,
   };
   return clone(child_func, ca.stack_ptr, CLONE_PARENT | SIGCHLD, &ca);
 }
 
-#endif  // NSEXEC_H
+#endif // NSEXEC_H
--- nsexec/test/nsexec_test.cpp
+++ nsexec/test/nsexec_test.cpp
@@ -5,10 +5,11 @@
 
 #include "nsexec.h"
 
-#define STAGE_PARENT  0
-#define STAGE_CHILD   1
+#define STAGE_PARENT 0
+#define STAGE_CHILD 1
 
-TEST(CloneParentTest, BasicTest) {
+TEST(CloneParentTest, BasicTest)
+{
   jmp_buf env;
   int ok, current_stage;
   int syncfd;
@@ -22,40 +23,40 @@ TEST(CloneParentTest, BasicTest) {
   switch (current_stage)
   {
   case STAGE_PARENT:
-    {
-        pid_t child_pid = -1, pid_from_child;
-        int size;
+  {
+    pid_t child_pid = -1, pid_from_child;
+    int size;
 
-        // Create child process
-        child_pid = clone_parent(&env, STAGE_CHILD);
-        // Open sync_pipe
-        syncfd = sync_pipe[1];
-        ok = close(sync_pipe[0]);
-        EXPECT_GE(ok, 0);
-        size = read(syncfd, &pid_from_child, sizeof(pid_t));
-        EXPECT_EQ(size, sizeof(pid_t));
-        EXPECT_EQ(pid_from_child, child_pid);
-        EXPECT_NE(getpid(), pid_from_child);
-        EXPECT_EQ(getpid(), pid_from_child-1);
-    }
-    break;
+    // Create child process
+    child_pid = clone_parent(&env, STAGE_CHILD);
+    // Open sync_pipe
+    syncfd = sync_pipe[1];
+    ok = close(sync_pipe[0]);
+    EXPECT_GE(ok, 0);
+    size = read(syncfd, &pid_from_child, sizeof(pid_t));
+    EXPECT_EQ(size, sizeof(pid_t));
+    EXPECT_EQ(pid_from_child, child_pid);
+    EXPECT_NE(getpid(), pid_from_child);
+    EXPECT_EQ(getpid(), pid_from_child - 1);
+  }
+  break;
 
   case STAGE_CHILD:
-    {
-        int size;
-        pid_t process_id;
-        /* We're in a child and thus need to tell the parent if we die. */
-        syncfd = sync_pipe[0];
-        ok = close(sync_pipe[1]);
-        EXPECT_GE(ok, 0);
-        /* Create new user namespace. */
-        ok = unshare(CLONE_NEWUSER);
-        EXPECT_GE(ok, 0);
-        process_id = getpid();
-        size = write(syncfd, &process_id, sizeof(pid_t));
-        EXPECT_EQ(size, sizeof(pid_t));
-        exit(0);
-    }
-    break;
+  {
+    int size;
+    pid_t process_id;
+    /* We're in a child and thus need to tell the parent if we die. */
+    syncfd = sync_pipe[0];
+    ok = close(sync_pipe[1]);
+    EXPECT_GE(ok, 0);
+    /* Create new user namespace. */
+    ok = unshare(CLONE_NEWUSER);
+    EXPECT_GE(ok, 0);
+    process_id = getpid();
+    size = write(syncfd, &process_id, sizeof(pid_t));
+    EXPECT_EQ(size, sizeof(pid_t));
+    exit(0);
+  }
+  break;
   }
 }
